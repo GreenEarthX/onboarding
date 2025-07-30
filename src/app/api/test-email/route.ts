28080e9d03bb9e49fec4f6bfc7ec3dc70eb385b5
Mohamed Ben Kedim <mohamedbenkedim@gmail.com>
2025-07-30 20:24:36 +0100

    fixed email test

--- src/app/api/test-email/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server';
-import { send2FACode } from '@/app/lib/email/email';
-
-export async function GET() {
-  await send2FACode('bentwannes@gmail.com', '123456');
-  return NextResponse.json({ message: 'Test email sent' });
-}
